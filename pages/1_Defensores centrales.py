f0b36eada3bb340a031a70c489824decdfb02ac3
datosargentinosjuniors <datosargentinosjuniors@gmail.com>
2025-10-12 22:00:07 -0300

    Update 1_Defensores centrales.py

--- pages/1_Defensores centrales.py
+++ pages/1_Defensores centrales.py
@@ -14,7 +14,7 @@ def quitar_tildes(texto):
 
 @st.cache_data
 def load_data():
-    return pd.read_excel('data/todos_defensoresCentrales_todos_2025.xlsx')
+    return pd.read_excel('data/todos_defensoresCentrales_todos_20252026.xlsx')
 
 def main():
     st.set_page_config(page_title="Defensores centrales", layout="wide")
